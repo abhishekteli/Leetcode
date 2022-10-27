674747c070d5a5b10792c8df6797df2d94420b21
abhishekteli <ateli333@gmail.com>
2022-10-26 22:25:27 -0400

    Time: 851 ms (16.62%), Space: 0B (100.00%) - LeetHub

--- 0608-tree-node/0608-tree-node.sql
+++ 0608-tree-node/0608-tree-node.sql
@@ -2,11 +2,15 @@
 select id, "Root" as type 
 from Tree
 where p_id is null
+
 union all
+
 select id, "Inner" as type
 from Tree
 where id in (select p_id from Tree) and p_id in (select id from Tree where p_id is null or p_id is not null)
+
 union all
+
 select id, "Leaf" as type
 from Tree
 where id not in (select distinct p_id from Tree where p_id is not null) and p_id is not null
