fe6df77cd835b605878d8235b7dac9fa86b48407
abhishekteli <ateli333@gmail.com>
2022-11-09 18:08:12 -0500

    Time: 519 ms (96.80%), Space: 0B (100.00%) - LeetHub

--- 1393-capital-gain-loss/1393-capital-gain-loss.sql
+++ 1393-capital-gain-loss/1393-capital-gain-loss.sql
@@ -22,17 +22,24 @@
 # group by sell_buy_table.stock_name
 
 
- select sell_table.stock_name, (sell_table.price - buy_table.price) as capital_gain_loss 
- from 
- (
-     (select stock_name, sum(price) as price from Stocks where operation = "Sell" group by stock_name) as sell_table 
-     join 
-     (select stock_name, sum(price) as price from Stocks where operation = "Buy" group by stock_name) as buy_table 
-     on sell_table.stock_name = buy_table.stock_name
- )
- group by sell_table.stock_name
+ # select sell_table.stock_name, (sell_table.price - buy_table.price) as capital_gain_loss 
+ # from 
+ # (
+ #     (select stock_name, sum(price) as price from Stocks where operation = "Sell" group by stock_name) as sell_table 
+ #     join 
+ #     (select stock_name, sum(price) as price from Stocks where operation = "Buy" group by stock_name) as buy_table 
+ #     on sell_table.stock_name = buy_table.stock_name
+ # )
+ # group by sell_table.stock_name
 
-
+select stock_name, sum(
+case 
+    when operation = 'Buy' then -price
+    else price
+end
+) as capital_gain_loss
+from stocks
+group by stock_name
 
 #select price select sum(price) from Stocks where operation = "Sell" and stock_name = "Corona Masks";
 #select sum(price) from Stocks where operation = "Buy" and stock_name = "Corona Masks";
