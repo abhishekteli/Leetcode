250da8d64fdbe0e6e0169ef491b0240132503d69
abhishekteli <ateli333@gmail.com>
2022-11-09 18:05:09 -0500

    Time: 693 ms (47.25%), Space: 0B (100.00%) - LeetHub

---
+++ 1393-capital-gain-loss/1393-capital-gain-loss.sql
@@ -0,0 +1,38 @@
+# Write your MySQL query statement below
+
+
+
+# select sell_buy_table.stock_name, sum(sell_buy_table.price) - sum(sell_buy_table.price) as capital_gain_loss
+# from 
+# (
+#     (
+#         (
+#             select * 
+#             from Stocks
+#             where operation = "Sell"
+#         ) as sell_table join 
+#         (
+#             select * 
+#             from Stocks
+#             where operation = "Buy"
+#         ) 
+#         as buy_table on sell_table.stock_name = buy_table.stock_name
+#     )
+# ) as sell_buy_table
+# group by sell_buy_table.stock_name
+
+
+ select sell_table.stock_name, (sell_table.price - buy_table.price) as capital_gain_loss 
+ from 
+ (
+     (select stock_name, sum(price) as price from Stocks where operation = "Sell" group by stock_name) as sell_table 
+     join 
+     (select stock_name, sum(price) as price from Stocks where operation = "Buy" group by stock_name) as buy_table 
+     on sell_table.stock_name = buy_table.stock_name
+ )
+ group by sell_table.stock_name
+
+
+
+#select price select sum(price) from Stocks where operation = "Sell" and stock_name = "Corona Masks";
+#select sum(price) from Stocks where operation = "Buy" and stock_name = "Corona Masks";
